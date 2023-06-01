ac1e08170a060643213da92982f5decce56d78e0
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2023-06-01 13:49:07 +0200

    Changed initialization of benchmark instances
    Do not run install_tool.sh for randgen, as it installs outdated pip packages

--- vnncomp/scripts/benchmark/initialize_instance.sh
+++ vnncomp/scripts/benchmark/initialize_instance.sh
@@ -11,14 +11,27 @@ ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/vnncomp.pem ubuntu@${benchmark
             && sudo swapon /swapfile \
             && sudo apt-get install -y python3 python3-pip unzip git-lfs libgl1-mesa-glx ffmpeg libsm6 libxext6 \
             && sudo python3 -m pip install --upgrade pip \
-            && sudo python3 -m pip install torch==1.10.2 tensorflow==2.8.0 torchvision==0.11.3 onnxruntime==1.10.0 matplotlib==3.5.1 mxnet==1.9.1 opencv-python==4.7.0.72 pandas==2.0.2 \
+            && sudo python3 -m pip install \
+                matplotlib==3.5.1 \
+                mxnet==1.9.1 \
+                onnx==1.14.0 \
+                onnxruntime==1.15.0 \
+                opencv-python==4.7.0.72 \
+                pandas==2.0.2 \
+                psmisc==0.0.2 \
+                protobuf==3.20.0 \
+                scipy==1.10.1 \
+                skl2onnx==1.14.1 \
+                tensorflow==2.8.0 \
+                torch==1.10.2 \
+                torchvision==0.11.3 \
             && sudo python3 -m pip freeze \
             && git clone https://github.com/stanleybak/simple_adversarial_generator.git randgen \
             && cd randgen \
             && git checkout 34682c72681b8185ea5e448af22cb5dd3652b504 \
             && cd vnncomp_scripts \
-            && sudo ./install_tool.sh v1 \
-            && sudo python3 -m pip install protobuf==3.20.0 \
             && curl --retry 100 --retry-connrefused https://vnncomp.christopher-brix.de/update/${benchmark_id}/success \
             || curl --retry 100 --retry-connrefused https://vnncomp.christopher-brix.de/update/${benchmark_id}/failure \
     ' > logs/initialize_instance.log 2>&1 "
+# Importantly, we de not execute `sudo ./install_tool.sh v1` in `vnncomp_scripts`
+# It would do nothing but install outdated pip packages
