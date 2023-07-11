b56c0a73f192fad0666b9f84cec2f1110b666f27
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2023-07-12 01:25:06 +0200

    Fix for result upload

--- vnncomp/scripts/toolkit/github_export.sh
+++ vnncomp/scripts/toolkit/github_export.sh
@@ -8,23 +8,23 @@ ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/vnncomp.pem ubuntu@${benchmark
             ssh-keyscan github.com >> ~/.ssh/known_hosts \
                 && rm -rf all_results \
                 && git clone git@github.com:ChristopherBrix/vnncomp2023_results.git all_results \
-                && rm -rf all_results/${tool_name}/${benchmark_benchmark_year}_${benchmark_name} \
-                && mkdir -p all_results/${tool_name}/${benchmark_benchmark_year}_${benchmark_name} \
-                && touch all_results/${tool_name}/${benchmark_benchmark_year}_${benchmark_name}/.gitkeep
-            cp logs/counterexamples/${benchmark_benchmark_year}_${benchmark_name}/${benchmark_name}/* all_results/${tool_name}/${benchmark_benchmark_year}_${benchmark_name}/
-            cp logs/results_${benchmark_benchmark_year}_${benchmark_name}.csv all_results/${tool_name}/${benchmark_benchmark_year}_${benchmark_name}/results.csv \
+                && rm -rf all_results/${tool_name}/${benchmark_year}_${benchmark_name} \
+                && mkdir -p all_results/${tool_name}/${benchmark_year}_${benchmark_name} \
+                && touch all_results/${tool_name}/${benchmark_year}_${benchmark_name}/.gitkeep
+            cp logs/counterexamples/${benchmark_year}_${benchmark_name}/${benchmark_name}/* all_results/${tool_name}/${benchmark_year}_${benchmark_name}/
+            cp logs/results_${benchmark_year}_${benchmark_name}.csv all_results/${tool_name}/${benchmark_year}_${benchmark_name}/results.csv \
                 && cd all_results
-            gzip ${tool_name}/${benchmark_benchmark_year}_${benchmark_name}/*.counterexample
+            gzip ${tool_name}/${benchmark_year}_${benchmark_name}/*.counterexample
             ./SCORING/merge_results_per_team.sh
             git config --global user.name \\\\\\\"VNN-Comp Bot\\\\\\\" \
                 && git config --global user.email \\\\\\\"brix@cs.rwth-aachen.de\\\\\\\" \
-                && git add ${tool_name}/${benchmark_benchmark_year}_${benchmark_name} \
+                && git add ${tool_name}/${benchmark_year}_${benchmark_name} \
                 && git add ${tool_name}/results.csv \
                 && git pull \
                 && git status \
-                && git commit -m \\\\\\\"Added results for ${tool_name} on ${benchmark_benchmark_year}_${benchmark_name}\\\\\\\" \
+                && git commit -m \\\\\\\"Added results for ${tool_name} on ${benchmark_year}_${benchmark_name}\\\\\\\" \
                 && git push \
                 && curl --retry 100 --retry-connrefused ${ROOT_URL}/update/${benchmark_id}/success \
                 || curl --retry 100 --retry-connrefused ${ROOT_URL}/update/${benchmark_id}/failure
-        \\\" > >(tee logs/github_export_${benchmark_benchmark_year}_${benchmark_name}.log) 2>&1 < /dev/null
+        \\\" > >(tee logs/github_export_${benchmark_year}_${benchmark_name}.log) 2>&1 < /dev/null
     \" "
