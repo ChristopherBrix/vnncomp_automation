c509488bcb4c48af4aebdd01c6bdfe84748b84c6
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2023-07-14 19:33:28 +0200

    Postprocess results file to include correct paths

--- vnncomp/scripts/toolkit/run.sh
+++ vnncomp/scripts/toolkit/run.sh
@@ -13,6 +13,7 @@ ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/vnncomp.pem ubuntu@${benchmark
                 && git pull \
                 && mkdir -p ~/logs/counterexamples/${benchmark_year}_${benchmark_name} \
                 && ${sudo} env SHELLOPTS=xtrace /bin/bash run_all_categories.sh v1 ~/toolkit/${script_dir} . ~/logs/results_${benchmark_year}_${benchmark_name}.csv ~/logs/counterexamples/${benchmark_year}_${benchmark_name} ${benchmark_name} ${run_networks} \
+                && sed -i \\\\\\\"s/\\\\\\\\.\\\\\\\\/benchmarks/vnncomp${benchmark_year}_benchmarks\\\\\\\\/benchmarks/g\\\\\\\" ~/logs/results_${benchmark_year}_${benchmark_name}.csv \
                 && curl --retry 100 --retry-connrefused ${ROOT_URL}/update/${benchmark_id}/success \
                 || curl --retry 100 --retry-connrefused ${ROOT_URL}/update/${benchmark_id}/failure
         \\\" > >(tee logs/run_${benchmark_year}_${benchmark_name}.log) 2>&1 < /dev/null
