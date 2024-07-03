3436c5edcf553f57f808a61a373a7f62aad8c813
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-07-03 17:36:49 +0200

    Do not cut of logs

--- vnncomp/scripts/get_logs.sh
+++ vnncomp/scripts/get_logs.sh
@@ -2,4 +2,4 @@
 
 # trim result to 1MB
 ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/vnncomp.pem -o ConnectTimeout=10 ubuntu@${benchmark_ip} \
-    "tail -c 100000 logs/${log_name} 2> /dev/null || true;" || true
+    "cat logs/${log_name} 2> /dev/null || true;" || true
