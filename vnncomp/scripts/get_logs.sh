1d183a611c5992db563a4f93eaf92c3bc11f276b
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-07-08 12:28:02 +0200

    Truncate logs

--- vnncomp/scripts/get_logs.sh
+++ vnncomp/scripts/get_logs.sh
@@ -2,4 +2,4 @@
 
 # trim result to 1MB
 ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/vnncomp.pem -o ConnectTimeout=10 ubuntu@${benchmark_ip} \
-    "cat logs/${log_name} 2> /dev/null || true;" || true
+    "tail --bytes 1000000 logs/${log_name} 2> /dev/null || true;" || true
