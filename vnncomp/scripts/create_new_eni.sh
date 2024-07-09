0efe97884087884e392491c6b3f279d99f81cc32
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-07-09 22:15:56 +0200

    Set correct security group for ENIs

--- vnncomp/scripts/create_new_eni.sh
+++ vnncomp/scripts/create_new_eni.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # subnet us-west-2a
-aws ec2 create-network-interface --description ${user} --subnet-id subnet-0bebd3ab62946033d 
+# security group "sshonly"
+aws ec2 create-network-interface --description ${user} --subnet-id subnet-0bebd3ab62946033d --groups sg-0ae97ad442c2ea17d
