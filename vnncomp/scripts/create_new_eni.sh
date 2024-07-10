7cadc29490adc750fffea83af8c4f6592102b127
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-07-10 19:36:05 +0200

    Updated security group id

--- vnncomp/scripts/create_new_eni.sh
+++ vnncomp/scripts/create_new_eni.sh
@@ -2,4 +2,4 @@
 
 # subnet us-west-2a, specific to the AWS account.
 # security group "sshonly"
-aws ec2 create-network-interface --description ${user} --subnet-id subnet-a48e22dc --groups sg-0463a6fed8f974e73
+aws ec2 create-network-interface --description ${user} --subnet-id subnet-a48e22dc --groups sg-0d2f7541ef43bdb35
