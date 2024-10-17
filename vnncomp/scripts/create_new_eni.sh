609d1cebabd36ea07ec9cf73dd41119f1ebdb8e6
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-10-17 05:27:17 +0200

    Update eni creation process to new AWS account

--- vnncomp/scripts/create_new_eni.sh
+++ vnncomp/scripts/create_new_eni.sh
@@ -2,4 +2,4 @@
 
 # subnet us-west-2a, specific to the AWS account.
 # security group "sshonly"
-aws ec2 create-network-interface --description ${user} --subnet-id subnet-a48e22dc --groups sg-0d2f7541ef43bdb35
+aws ec2 create-network-interface --description ${user} --subnet-id subnet-0bebd3ab62946033d --groups sg-0ae97ad442c2ea17d
